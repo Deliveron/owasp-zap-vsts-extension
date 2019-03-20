7b5edd2442a4b4df9a142434f9c80b87cc931ecc
Mike Douglas <mikedouglas@deliveron.com>
2019-03-20 00:02:27 -0500

    added new ACI scan script, a unit test project, and some refactoring

---
+++ IntegrationTests/OwaspZapTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using owasp_zap_vsts_tool.Services;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class OwaspZapTests
+    {
+        [TestMethod]
+        public async Task OwaspZapFailures_ShouldCreateBugs()
+        {
+
+            string collectionUri = "https://myaccount.visualstudio.com";
+            string teamProjectName = "My Team Project";
+            string team = "SRE";
+            string releaseUri = "vstfs:///ReleaseManagement/Release/209";
+            string releaseEnvironmentUri = "vstfs:///ReleaseManagement/Environment/538";
+            string filePath = @"C:\alerts\full_OwaspZapAlerts.xml";
+            bool failOnHigh = false;
+            string personalAccessToken = "1234";
+            string targetUrl = "https://mysite.azurewebsites.net";
+            string bugTitlePrefix = "Unit Test";
+
+
+            var client = new AzureDevOpsClient();
+
+           await client.CreateBugFromPenTestAsync(collectionUri, teamProjectName, team, releaseUri, releaseEnvironmentUri, filePath, failOnHigh, personalAccessToken, targetUrl, bugTitlePrefix);
+        }
+
+    }
+}
---
+++ IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa46ca21-961f-43ee-9852-023e163e8da1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
